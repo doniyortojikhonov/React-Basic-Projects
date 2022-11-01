5adf6a8fae5b9822d324802bbdaa5e117a68a0b5
Doniyor Tojikhonov <tojikhonov.doniyor@gmail.com>
2022-11-01 19:45:45 +0900

    Final Commit

---
+++ 01-birthday-reminder/build/precache-manifest.14a687a5d612d6ba74d79c4a38d3437f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910771426be34e63c5e7acc19524ce93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb1422ca01062d16dcca",
+    "url": "/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb1422ca01062d16dcca",
+    "url": "/static/js/main.d5b03ab1.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
