5b9aa5a6a0b0d3ba6a2b5e797f9cc8ad0c601a15
Doniyor Tojikhonov <tojikhonov.doniyor@gmail.com>
2022-11-09 20:11:16 +0900

    build folder added

---
+++ 03-reviews/build/precache-manifest.172bcddf78a47439b9ba1a69160f31ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d9aca53d32d445aa88a28350fe2d6ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e576a05c88bd42db8fc8",
+    "url": "/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e576a05c88bd42db8fc8",
+    "url": "/static/js/main.83259026.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
