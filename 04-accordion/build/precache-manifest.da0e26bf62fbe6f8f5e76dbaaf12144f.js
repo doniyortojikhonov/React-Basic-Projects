b98468fc920a60e94e5d828607bb70a6c99b3b34
Doniyor Tojikhonov <tojikhonov.doniyor@gmail.com>
2022-11-10 21:20:37 +0900

    Final Commit

---
+++ 04-accordion/build/precache-manifest.da0e26bf62fbe6f8f5e76dbaaf12144f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d40e3ee1d2c976cfb06f9291ff5abc0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e2920e584df9ddf4a12",
+    "url": "/static/css/main.4b589e59.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e2920e584df9ddf4a12",
+    "url": "/static/js/main.24ebe048.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
