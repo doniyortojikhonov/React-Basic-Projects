78294c7dcf53aa403db32b933060984e8ec49396
Doniyor Tojikhonov <tojikhonov.doniyor@gmail.com>
2022-11-12 19:11:27 +0900

    Final Commit

---
+++ 05-menu/build/precache-manifest.c8c75fae5d75eef13dfef2f6962c3b20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f402fae55f1d57ff984237705ef6fba8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32e4afe84d30e153a01f",
+    "url": "/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32e4afe84d30e153a01f",
+    "url": "/static/js/main.a4ade1f1.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
