64d51918b2815714aa3e06a5c1cf73ef377bfbb5
Doniyor Tojikhonov <tojikhonov.doniyor@gmail.com>
2022-11-16 00:00:54 +0900

    Final commit

---
+++ 07-slider/build/precache-manifest.9ecf971e4c7f383e251fdb680f1153d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86f64fd7832755e45267318dbcd77d81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec6cae179d54492011e8",
+    "url": "/static/css/main.b81d78ed.chunk.css"
+  },
+  {
+    "revision": "e1c4e21724a78a9e8542",
+    "url": "/static/js/2.2c3ffba9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c3ffba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec6cae179d54492011e8",
+    "url": "/static/js/main.4b1df055.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
