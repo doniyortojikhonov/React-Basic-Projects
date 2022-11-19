758c9fadf632c979dbee930dcf408e544e22a37c
Doniyor Tojikhonov <tojikhonov.doniyor@gmail.com>
2022-11-19 21:02:06 +0900

    third commit

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -6,7 +6,14 @@ function App() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("Hello World");
+        let amount = parseInt(count);
+        if (count <= 0) {
+            amount = 1;
+        }
+        if (count > 8) {
+            amount = 8;
+        }
+        setText(data.slice(0, amount));
     };
     return (
         <section className="section-center">
@@ -25,16 +32,9 @@ function App() {
                 </button>
             </form>
             <article className="lorem-text">
-                <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Perferendis, tempore aspernatur. Veniam aut quae nam
-                    suscipit officia dolores dignissimos quaerat.{" "}
-                </p>
-                <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Perferendis, tempore aspernatur. Veniam aut quae nam
-                    suscipit officia dolores dignissimos quaerat.{" "}
-                </p>
+                {text.map((item, index) => {
+                    return <p key={index}>{item}</p>;
+                })}
             </article>
         </section>
     );
