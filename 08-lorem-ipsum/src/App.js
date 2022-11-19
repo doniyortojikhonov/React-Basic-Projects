07f47b98860eb01fb85ce79cfe8f4d1d4d732a07
Doniyor Tojikhonov <tojikhonov.doniyor@gmail.com>
2022-11-19 20:48:36 +0900

    first commit

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -1,11 +1,43 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    const [count, setCount] = useState(0);
+    const [text, setText] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Hello World");
+    };
+    return (
+        <section className="section-center">
+            <h3>tired of boring lorem ipsum?</h3>
+            <form className="lorem-form" onSubmit={handleSubmit}>
+                <label htmlFor="amount">Paragraphs:</label>
+                <input
+                    type="number"
+                    name="amount"
+                    id="amount"
+                    value={count}
+                    onChange={(e) => setCount(e.target.value)}
+                />
+                <button type="submit" className="btn">
+                    generate
+                </button>
+            </form>
+            <article className="lorem-text">
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Perferendis, tempore aspernatur. Veniam aut quae nam
+                    suscipit officia dolores dignissimos quaerat.{" "}
+                </p>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Perferendis, tempore aspernatur. Veniam aut quae nam
+                    suscipit officia dolores dignissimos quaerat.{" "}
+                </p>
+            </article>
+        </section>
+    );
 }
 
 export default App;
