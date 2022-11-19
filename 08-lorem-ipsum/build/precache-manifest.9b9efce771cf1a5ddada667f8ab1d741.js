aaeac5b0f7abd38c7e57e73ccaa3264d5eb13b33
Doniyor Tojikhonov <tojikhonov.doniyor@gmail.com>
2022-11-19 21:02:58 +0900

    Final commit

---
+++ 08-lorem-ipsum/build/precache-manifest.9b9efce771cf1a5ddada667f8ab1d741.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a48e5f2161fd4c5938e3670274872cfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20e9461767faa680f515",
+    "url": "/static/css/main.fb86bce3.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e9461767faa680f515",
+    "url": "/static/js/main.1ccf5326.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
