61001d5b9b53a84103ee628b4cbdfe37a136588d
Doniyor Tojikhonov <tojikhonov.doniyor@gmail.com>
2022-11-20 20:15:58 +0900

    third commit

--- 09-color-generator/src/App.js
+++ 09-color-generator/src/App.js
@@ -12,6 +12,7 @@ function App() {
         e.preventDefault();
         try {
             let colors = new Values(color).all(10);
+            setList(colors);
         } catch (error) {
             setError(true);
         }
@@ -35,7 +36,9 @@ function App() {
                 </form>
             </section>
             <section className="colors">
-                <h4>list goes here</h4>
+                {list.map((color, index) => {
+                    return <SingleColor key={index} {...color} index={index} />;
+                })}
             </section>
         </React.Fragment>
     );
