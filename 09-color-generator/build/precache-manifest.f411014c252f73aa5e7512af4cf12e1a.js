723c5d413d33556d4d67921053992daea462a0ab
Doniyor Tojikhonov <tojikhonov.doniyor@gmail.com>
2022-11-20 20:29:15 +0900

    Final Commit

---
+++ 09-color-generator/build/precache-manifest.f411014c252f73aa5e7512af4cf12e1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ca0f69c3f08d1bb68ea51f0586dcbaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "598306c9a140d32eb441",
+    "url": "/static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "598306c9a140d32eb441",
+    "url": "/static/js/main.8aaa0af7.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
