f9b2921c18a5a36f48ad27b0a104a1de3a45b3a2
Doniyor Tojikhonov <tojikhonov.doniyor@gmail.com>
2022-11-24 08:10:18 +0900

    edit item functionality added

--- 10-grocery-bud/src/App.js
+++ 10-grocery-bud/src/App.js
@@ -19,6 +19,18 @@ function App() {
             showAlert(true, "danger", "please enter value");
         } else if (name && isEditing) {
             // deal with edit
+            setList(
+                list.map((item) => {
+                    if (item.id === editID) {
+                        return { ...item, title: name };
+                    }
+                    return item;
+                })
+            );
+            setName("");
+            setEditID(null);
+            setIsEditing(false);
+            showAlert(true, "success", "value changed");
         } else {
             // show alert
             showAlert(true, "success", "item added to the list");
@@ -42,9 +54,20 @@ function App() {
         setList([]);
     };
 
+    // Single Item removing functionality
+
     const removeItem = (id) => {
         showAlert(true, "danger", "item removed");
         setList(list.filter((item) => item.id !== id));
+    };
+
+    // Editing single item functionality
+
+    const editItem = (id) => {
+        const specificItem = list.find((item) => item.id === id);
+        setIsEditing(true);
+        setEditID(id);
+        setName(specificItem.title);
     };
 
     return (
@@ -69,7 +92,11 @@ function App() {
             </form>
             {list.length > 0 && (
                 <div className="grocery-container">
-                    <List items={list} removeItem={removeItem} />
+                    <List
+                        items={list}
+                        removeItem={removeItem}
+                        editItem={editItem}
+                    />
                     <button className="clear-btn" onClick={clearList}>
                         clear items
                     </button>
