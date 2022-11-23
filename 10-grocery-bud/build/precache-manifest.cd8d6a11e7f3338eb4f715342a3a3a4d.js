ceb063a5f4022919e3a69b1f8d793125bb7d098b
Doniyor Tojikhonov <tojikhonov.doniyor@gmail.com>
2022-11-24 08:25:04 +0900

    Final commit

---
+++ 10-grocery-bud/build/precache-manifest.cd8d6a11e7f3338eb4f715342a3a3a4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0655e86873acd06393dd223b5ab889c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b503d14ef3862717d68",
+    "url": "/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b503d14ef3862717d68",
+    "url": "/static/js/main.126a7c58.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
